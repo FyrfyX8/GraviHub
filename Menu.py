965273ba457221fa80fdb614de318de467b2e38a
FyrfyX8 <f.norbisrath@web-n-office.de>
2023-09-10 16:35:38 +0200

    Added more code to Menu.py

--- Menu.py
+++ Menu.py
@@ -8,37 +8,53 @@ import asyncio
 from gravitraxconnect import gravitrax_bridge as gb
 from encoder import Encoder
 
+def puttonPress(arg):
+    print("Pressed")
+
+
 GPIO.setmode(GPIO.BCM)
 
 buttonPin = 27
 
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(buttonPin, GPIO.RISING, callback=puttonPress, bouncetime=200)
 
 CursorPos = 0
 MenuIndex = 0
+MenuMaxIndex = 0
+MenuPos = 0
+MenuMaxPos = 0
 MacAdresses = ["new"]
 SlotsMM = ["add new Connection"]
 SlotsCM = ["Main Menu","Start Skript","Disconnect","Delete Connection"]
 MainMenu = [SlotsMM, MacAdresses]
 MenuDeph = 0
 def valueChanged(value, direction):
-    global MenuIndex, CursorPos, CursorPrPos
+    global MenuPos, CursorPos, MenuMaxPos, MenuIndex
     CursorPrPos = CursorPos
+    MenuPrPos = MenuPos
+
     if direction == "L" :
-        CursorPos -= 1
+        if MenuIndex != 0:
+            CursorPos -= 1
+            MenuIndex -= 1
     else:
-        CursorPos += 1
+        if MenuIndex != MenuMaxIndex:
+            CursorPos += 1
+            MenuIndex += 1
 
     if CursorPos == -1:
         CursorPos = 0
-        MenuIndex -=1
+        if MenuPos != 0:
+            MenuPos -=1
     elif CursorPos == 4:
         CursorPos = 3
-        MenuIndex += 1
-    if CursorPos == CursorPrPos:
-        pass
-    else:
+        if MenuPos != MenuMaxPos:
+            MenuPos += 1
+    if CursorPos != CursorPrPos:
         Cursor(CursorPrPos)
+    if MenuPos != MenuPrPos:
+        Menu()
 
     print(CursorPos, MenuIndex)
 
@@ -56,7 +72,7 @@ lcd.clear()
 
 lcd.cursor_pos = (1, 0)
 lcd.write_string('Welcome to GraviHub!')
-sleep(2)
+sleep(5)
 try:
     with open("Connections.json","r") as Connections:
         json_str = json.load(Connections)
@@ -73,26 +89,17 @@ try:
 except FileNotFoundError:
     print("Connections.json does not exist creating new one!")
     with open("Connections.json", "w") as Connections:
-        json.dump(["1", "2", "3"], Connections)
+        json.dump(["Connection1", "Connection2", "Connection3"], Connections)
 
 
 previous_button_state = GPIO.input(buttonPin)
 
-def puttonPress():
-    global MenuIndex
-    button_state = GPIO.input(buttonPin)
-    if button_state != previous_button_state:
-        previous_button_state = button_state
-        if button_state == GPIO.HIGH:
-            print("Pressed")
-            return MenuIndex
-
 def Cursor(CursorPrPos):
     global CursorPos
-    lcd.cursor_pos = (CursorPos, 0)
-    lcd.write_string(">")
     lcd.cursor_pos = (CursorPrPos, 0)
     lcd.write_string(" ")
+    lcd.cursor_pos = (CursorPos, 0)
+    lcd.write_string(">")
 
 def ResetCursor():
     global CursorPos
@@ -100,8 +107,34 @@ def ResetCursor():
     CursorPos = 0
     Cursor(CursorPrPos)
 
+def MenuLenght(MenuType):
+    global MenuMaxIndex, MenuMaxPos
+    MenuMaxIndex = len(MenuType) - 1
+    MenuMaxPos = MenuMaxIndex - 3
+    print("Menu Lenght: ", MenuMaxIndex,", Max Pos: ", MenuMaxPos)
+
+def Menu():
+    global MenuDeph, SlotsCM, SlotsMM, MenuPos
+    if MenuDeph == 0:
+        CurrentSlots = SlotsMM
+    elif MenuDeph == 1:
+        CurrentSlots = SlotsCM
+    else:
+        pass
+    MenuLenght(CurrentSlots)
+    CurrentIndex = MenuPos
+    CurrentRow = 0
+    lcd.clear()
+    for i in range(4):
+        lcd.cursor_pos = (CurrentRow, 1)
+        CurrentRow += 1
+        lcd.write_string(CurrentSlots[CurrentIndex])
+        CurrentIndex += 1
+    Cursor(0)
+
 lcd.clear()
 Cursor(1)
+Menu()
 
 while True:
-    pass
+    time.sleep(0.01)
