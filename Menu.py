161e9400ce669fe7e9b9634f5477be83ee6a5da4
FyrfyX8 <f.norbisrath@web-n-office.de>
2023-09-11 20:40:24 +0200

    Added more code to Menu.py

--- Menu.py
+++ Menu.py
@@ -1,6 +1,5 @@
 import time
 import json
-
 from RPLCD.i2c import CharLCD
 import RPi.GPIO as GPIO
 from time import sleep
@@ -8,16 +7,50 @@ import asyncio
 from gravitraxconnect import gravitrax_bridge as gb
 from encoder import Encoder
 
-def puttonPress(arg):
-    print("Pressed")
+async def GB():
+    global MenuDeph, MenuIndex, MacAdresses, Disconnecting
+    lcd.clear()
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string("Connecting To Bridge")
+    if await bridge.connect(timeout=25):
+        lcd.clear()
+        del MacAdresses[MenuIndex]
+        MacAdresses.insert(MenuIndex, bridge.get_address())
+        print(MacAdresses)
+        ResetMenu()
+        while bridge.is_connected():
+            asyncio.sleep(1)
+            print("still Connectet")
+    else:
+        lcd.clear()
+        lcd.cursor_pos = (1, 0)
+        lcd.write_string("Connection not found")
+        time.sleep(2)
+        lcd.clear()
+        MenuDeph = 0
+        ResetMenu()
 
+
+
+def puttonPress(arg):
+    global MenuDeph, MenuIndex, MenuMaxIndex, MacAdresses, Disconnecting
+    MenuPrDeph = MenuDeph
+    if MenuDeph == 0:
+        if MenuIndex != MenuMaxIndex:
+            MenuDeph = 1
+            asyncio.run(GB())
+    elif MenuDeph == 1:
+        if MenuIndex == 0:
+            MenuDeph = 0
+    if MenuDeph != MenuPrDeph:
+        ResetMenu()
 
 GPIO.setmode(GPIO.BCM)
 
 buttonPin = 27
 
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(buttonPin, GPIO.RISING, callback=puttonPress, bouncetime=200)
+GPIO.add_event_detect(buttonPin, GPIO.RISING, callback=puttonPress, bouncetime=600)
 
 CursorPos = 0
 MenuIndex = 0
@@ -29,6 +62,7 @@ SlotsMM = ["add new Connection"]
 SlotsCM = ["Main Menu","Start Skript","Disconnect","Delete Connection"]
 MainMenu = [SlotsMM, MacAdresses]
 MenuDeph = 0
+
 def valueChanged(value, direction):
     global MenuPos, CursorPos, MenuMaxPos, MenuIndex
     CursorPrPos = CursorPos
@@ -58,6 +92,29 @@ def valueChanged(value, direction):
 
     print(CursorPos, MenuIndex)
 
+
+def ReadConnections():
+    try:
+        with open("Connections.json", "r") as Connections:
+            json_str = json.load(Connections)
+            for I in reversed(json_str):
+                SlotsMM.insert(0, I)
+
+            for I in SlotsMM:
+                if I == "add new Connection":
+                    pass
+                else:
+                    MacAdresses.insert(0, "none")
+            print(SlotsMM)
+            print(MacAdresses)
+            Connections.close()
+    except FileNotFoundError:
+        print("Connections.json does not exist creating new one!")
+        with open("Connections.json", "w") as Connections:
+            json.dump(["Connection1", "Connection2", "Connection3"], Connections)
+            Connections.close()
+        ReadConnections()
+
 e1 = Encoder(4,17, valueChanged)
 bridge = gb.Bridge()
 
@@ -72,26 +129,9 @@ lcd.clear()
 
 lcd.cursor_pos = (1, 0)
 lcd.write_string('Welcome to GraviHub!')
-sleep(5)
-try:
-    with open("Connections.json","r") as Connections:
-        json_str = json.load(Connections)
-        for I in reversed(json_str):
-            SlotsMM.insert(0, I)
+ReadConnections()
+sleep(3)
 
-        for I in SlotsMM:
-            if I == "add new Connection":
-                pass
-            else:
-                MacAdresses.insert(0,"none")
-        print(SlotsMM)
-        print(MacAdresses)
-        Connections.close()
-except FileNotFoundError:
-    print("Connections.json does not exist creating new one!")
-    with open("Connections.json", "w") as Connections:
-        json.dump(["Connection1", "Connection2", "Connection3"], Connections)
-        Connections.close()
 
 
 previous_button_state = GPIO.input(buttonPin)
@@ -131,9 +171,16 @@ def Menu():
         lcd.write_string(CurrentSlots[CurrentIndex]+ " "*(19 -len (CurrentSlots[CurrentIndex])))
         CurrentIndex += 1
 
+def ResetMenu():
+    global MenuIndex, MenuPos
+    MenuIndex = 0
+    MenuPos = 0
+    ResetCursor()
+    Menu()
+
 lcd.clear()
 Cursor(1)
 Menu()
-
 while True:
-    time.sleep(0.01)
+    sleep(0.01)
+
