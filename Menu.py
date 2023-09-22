72088d6218cc00118bcb1ef07168924d8d026a6c
FyrfyX8 <f.norbisrath@web-n-office.de>
2023-09-23 00:56:26 +0200

    First Working version V.1

--- Menu.py
+++ Menu.py
@@ -8,6 +8,8 @@ import json
 from pathlib import Path
 import importlib.util
 
+# GraviHub V.1
+
 lcd = CharLCD(i2c_expander="PCF8574", address=0x27, port=1, cols=20, rows=4, dotsize=8)
 GPIO.setmode(GPIO.BCM)
 
