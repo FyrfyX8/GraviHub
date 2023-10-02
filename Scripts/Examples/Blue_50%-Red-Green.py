8d7fd6abce7bf87e2a87b34c2fdb5eb585f25504
FyrfyX8 <f.norbisrath@web-n-office.de>
2023-10-02 21:52:24 +0200

    Added Exemple Skripts

--- Scripts/Examples/Blue_50%-Red-Green.py
+++ Scripts/Examples/Blue_50%-Red-Green.py
@@ -1,17 +1,19 @@
 from gravitraxconnect import gravitrax_bridge as gb
 from gravitraxconnect import gravitrax_constants as gc
-#this script sends Red signals from Trigger to Switch
-#or from Goal-trigger to Dome-Stater
-
+import random
+#this script waits for blue signals and sends based on 50& Red or Green signals
 async def notification_callback(bridge: gb.Bridge, **signal): # Code runs when receving a signal
     if signal.get("Header") == gc.MSG_DEFAULT_HEADER:
         status = signal.get("Status")
         color = signal.get("Color")
         stone = signal.get("Stone")
-        if stone == gc.STONE_FINISH and color == gc.COLOR_RED:
-            await bridge.send_signal(status= gc.STATUS_STARTER, color_channel=gc.COLOR_RED, stone=gc.STONE_BRIDGE, resends=12)
-        if stone == gc.STONE_TRIGGER and color == gc.COLOR_RED:
-            await bridge.send_signal(status= gc.STATUS_SWITCH, color_channel=gc.COLOR_RED, stone=gc.STONE_BRIDGE, resends=12)
+        if stone == gc.STONE_FINISH and color == gc.COLOR_BLUE:
+            if random.random() < 0.5:
+                await bridge.send_signal(status=gc.STATUS_ALL, color_channel=gc.COLOR_RED, stone=gc.STONE_BRIDGE,
+                                         resends=12)
+            else:
+                await bridge.send_signal(status=gc.STATUS_ALL, color_channel=gc.COLOR_GREEN, stone=gc.STONE_BRIDGE,
+                                         resends=12)
 async def GBsetup(bridge, bridgeCount=None): # Code runs shortly after connecting
     if bridgeCount == 1:
         await bridge.start_bridge_mode()
--- Scripts/Template.py
+++ Scripts/Template.py
@@ -3,11 +3,11 @@ from gravitraxconnect import gravitrax_bridge as gb
 from gravitraxconnect import gravitrax_constants as gc
 
 
-async def notification_callback(bridge: gb.Bridge, **signal): # Code runs when receving a signal
-    # your code here
+async def notification_callback(bridge: gb.Bridge, **signal): #Code runs when receving a signal
+    #your code here
 
-async def GBsetup(bridge, bridgeCount=None): # Code runs shortly after connecting
-    # your code here
+async def GBsetup(bridge, bridgeCount=None): #Code runs shortly after connecting
+    #your code here
 
-async def GBshutdown(bridge, bridgeCount=None): # Code runs shortly before disconnecting
-    # your code here
+async def GBshutdown(bridge, bridgeCount=None): #Code runs shortly before disconnecting
+    #your code here
